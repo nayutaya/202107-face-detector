6c23520199b85af6da5990e45905a81ac5d5f6e5
Yuya Kato <yuyakato@gmail.com>
2021-07-27 02:37:11 +0900

    リファクタリング

--- browser/app/pages/index.js
+++ browser/app/pages/index.js
@@ -9,28 +9,27 @@ export default function Page() {
   // TODO: 結果を可視化する処理を実装する。
 
   const handleFile = (acceptedFiles) => {
-    if ( acceptedFiles.length >= 1 ) {
-      const imageFile = acceptedFiles[0];
-      const fileReader = new FileReader();
-      fileReader.addEventListener("load", () => {
-        // preview.src = fileReader.result;
-        setImage({
-          file: imageFile,
-          dataUrl: fileReader.result,
-        });
+    if ( acceptedFiles.length < 0 ) return;
+    const imageFile = acceptedFiles[0];
+    const fileReader = new FileReader();
+    fileReader.addEventListener("load", () => {
+      setImage({
+        file: imageFile,
+        dataUrl: fileReader.result,
+      });
 
-        const formData = new FormData();
-        formData.append("file", imageFile)
-        const param = {
-          method: "POST",
-          body: formData,
-        };
-        // TODO: replace to internal API.
-        fetch("http://192.168.1.204:8000/detect", param)
-          .then((result) => console.log({result}));
-      }, false);
-      fileReader.readAsDataURL(imageFile);
-    }
+      const formData = new FormData();
+      formData.append("file", imageFile)
+      const param = {
+        method: "POST",
+        body: formData,
+      };
+      // TODO: replace to internal API.
+      fetch("http://192.168.1.204:8000/detect", param)
+        .then((response) => response.json())
+        .then((result) => console.log({result}));
+    }, false);
+    fileReader.readAsDataURL(imageFile);
   }
 
   const backgroundStyle = (image == null ? {} :

36c2e5803b500e4a90b1220f7f4a3c2d9f657bd6
Yuya Kato <yuyakato@gmail.com>
2021-07-27 01:57:43 +0900

    ドラッグ＆ドロップしたファイルをプレビューする処理を追加

--- browser/app/pages/index.js
+++ browser/app/pages/index.js
@@ -1,26 +1,53 @@
 import Dropzone from "react-dropzone";
 import Head from "next/head";
+import { useState } from "react";
 
 export default function Page() {
-  // TODO: 画像ファイルを送信するフォームを追加する。
+  const [image, setImage] = useState(null);
+
   // TODO: 顔検出サーバを呼び出す処理を実装する。
   // TODO: 結果を可視化する処理を実装する。
+
+  const handleFile = (acceptedFiles) => {
+    if ( acceptedFiles.length >= 1 ) {
+      const imageFile = acceptedFiles[0];
+      const fileReader = new FileReader();
+      fileReader.addEventListener("load", () => {
+        // preview.src = fileReader.result;
+        setImage({
+          file: imageFile,
+          dataUrl: fileReader.result,
+        })
+      }, false);
+      fileReader.readAsDataURL(imageFile);
+    }
+  }
+
   return (
     <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+      <Dropzone onDrop={handleFile}>
         {({getRootProps, getInputProps}) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Drag "n" drop some files here, or click to select files</p>
-            </div>
-          </section>
+          <div
+              style={{
+                width: "200px",
+                height: "200px",
+                border: "1px dotted black",
+              }}
+              {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>JPEG形式の画像ファイルをここにドラッグ＆ドロップするか、クリックして画像ファイルを選択してください</p>
+          </div>
         )}
       </Dropzone>
+      {image == null ? null :
+        <div>
+          <img src={image.dataUrl} />
+        </div>
+      }
     </div>
   );
 }

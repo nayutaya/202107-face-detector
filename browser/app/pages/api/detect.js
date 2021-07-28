538c205f241be6595eec37eed99d224e7395e739
Yuya Kato <yuyakato@gmail.com>
2021-07-28 10:36:18 +0900

    リクエストを転送する処理を追加

--- browser/app/pages/api/detect.js
+++ browser/app/pages/api/detect.js
@@ -1,15 +1,27 @@
+import http from "http";
+
 export const config = {
   api: {
     bodyParser: false,
   },
 };
 
+// REF: https://github.com/rchipka/node-osmosis/blob/1d432e6c6b190a1a7bf415c62bb8ab0d7ce04e56/test/proxy.js#L8
 export default async function handler(req, res) {
+  const backend_req = http.request("http://detector-insightface:8000/detect", {
+    method: req.method,
+    headers: {
+      "content-length": req.headers["content-length"],
+      "content-type": req.headers["content-type"],
+    },
+  });
+  console.log({backend_req})
+
   // console.log({req});
+  // console.log(req.method);
+  // console.log(req.headers);
+  // console.log(req.body);
   /*
-  console.log(req.method);
-  console.log(req.headers);
-  console.log(req.body);
   if ( req.method == "POST" ) {
     const param = {
       method: "POST",
@@ -27,26 +39,30 @@ export default async function handler(req, res) {
     res.status(405).json({});
   }
   */
-  /*
-  req.addListener('data', function (chunk) {
-    // req.write(chunk, 'binary');
-    // console.log({chunk});
-    console.log("data");
+  backend_req.addListener("response", function (backend_res) {
+    console.log("backend_req.response");
+    backend_res.addListener("data", function (chunk) {
+      console.log("backend_res.data");
+      console.log({chunk});
+      res.write(chunk, "binary");
+    });
+    backend_res.addListener("end", function () {
+      console.log("backend_res.end");
+      res.end();
+    });
+    res.writeHead(backend_res.statusCode, backend_res.headers);
   });
-  req.addListener('end', function () {
+  req.addListener("data", function (chunk) {
+    console.log("req.data");
+    console.log({chunk});
+    backend_req.write(chunk, "binary");
+  });
+  req.addListener("end", function () {
     // res.status(200).json({});
-    console.log("end");
-    // req.end();
+    console.log("req.end");
+    backend_req.end();
   });
-  console.log("complete:", req.complete);
-  req.setEncoding('binary');
-  // console.log(req.body);
-
+  // const body = req.read();
+  // console.log(body.length);
   // res.status(200).json({});
-  res.setTimeout(1000, () => {
-    res.status(200).json({});
-  });
-  */
-  console.log(req.read());
-  res.status(200).json({});
 }

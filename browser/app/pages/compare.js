d1aaceff190b54852305d319dc10de5dbaf7f0bc
Yuya Kato <yuyakato@gmail.com>
2021-08-02 21:10:12 +0900

    画像をドラッグ＆ドロップする対象を追加

--- browser/app/pages/compare.js
+++ browser/app/pages/compare.js
@@ -1,4 +1,30 @@
+// import { useState } from "react";
+import Dropzone from "react-dropzone";
 import Head from "next/head";
+
+function ImageSelector() {
+  const handleFile = () => {
+  };
+  return (
+    <Dropzone onDrop={handleFile}>
+      {({getRootProps, getInputProps}) => (
+        <div
+            style={{
+              border: "2px dotted black",
+              padding: "5px",
+            }}
+          {...getRootProps()}>
+          <div
+              style={{
+                height: "200px",
+              }}>
+            <input {...getInputProps()} />
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  )
+}
 
 export default function Page() {
   return (
@@ -7,6 +33,31 @@ export default function Page() {
         <title>face-detector | compare</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div
+        style={{
+          position: "relative"
+        }}>
+        <div
+            style={{
+              position: "absolute",
+              left: "0px",
+              top: "0px",
+              width: "200px",
+              height: "150px",
+            }}>
+          <ImageSelector />
+        </div>
+        <div
+            style={{
+              position: "absolute",
+              left: "300px",
+              top: "0px",
+              width: "200px",
+              height: "150px",
+            }}>
+          <ImageSelector />
+        </div>
+      </div>
     </>
   );
 }

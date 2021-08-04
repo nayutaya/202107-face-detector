7df60e7a9a77c536fc405f26877cc8baf5f95fc4
Yuya Kato <yuyakato@gmail.com>
2021-08-05 02:26:47 +0900

    変数名を変更

--- detector-insightface/src/main.py
+++ detector-insightface/src/main.py
@@ -54,7 +54,7 @@ def compute_similarity(embedding1, embedding2):
 SERVICE = {
     "name": "detector-insightface",
     "version": "0.2.0",
-    "computeDevice": onnxruntime.get_device(),
+    "computingDevice": onnxruntime.get_device(),
     "libraries": {
         "insightface": insightface.__version__,
         "onnxruntime": onnxruntime.__version__,

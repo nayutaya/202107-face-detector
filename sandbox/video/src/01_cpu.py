74357d493f56d4a57049c72d969cb2312033fd00
Yuya Kato <yuyakato@gmail.com>
2021-08-05 22:22:18 +0900

    フレームの読み込み処理を計測する処理を追加

--- sandbox/video/src/01_cpu.py
+++ sandbox/video/src/01_cpu.py
@@ -12,15 +12,22 @@ face_analysis.prepare(ctx_id=0, det_size=(640, 640))
 capture = cv2.VideoCapture("pixabay_76889_960x540.mp4")
 assert capture.isOpened()
 
+total_read_frame_elapsed_ns = 0
+total_read_frame_count = 0
 total_detection_elapsed_ns = 0
 total_detection_count = 0
 
 # for frame_index in itertools.count():
 for frame_index in range(5):
+    start_ns = time.perf_counter_ns()
     result, frame = capture.read()
-    print((frame_index, result))
+    read_frame_elapsed_ns = time.perf_counter_ns() - start_ns
+    print((frame_index, result, read_frame_elapsed_ns))
     if not result:
         break
+
+    total_read_frame_elapsed_ns += read_frame_elapsed_ns
+    total_read_frame_count += 1
 
     start_ns = time.perf_counter_ns()
     faces = face_analysis.get(frame)
@@ -29,6 +36,13 @@ for frame_index in range(5):
 
     total_detection_elapsed_ns += detection_elapsed_ns
     total_detection_count += 1
+
+print("total_read_frame_elapsed_ns:", total_read_frame_elapsed_ns)
+print("total_read_frame_count:", total_read_frame_count)
+mean_read_frame_elapsed_ns = total_read_frame_elapsed_ns / total_read_frame_count
+print("mean_read_frame_elapsed_ms:", mean_read_frame_elapsed_ns / 1000 / 1000)
+read_frame_throughput_in_sec = 1_000_000_000 / mean_read_frame_elapsed_ns
+print("read_frame_throughput_in_sec:", read_frame_throughput_in_sec)
 
 print("total_detection_elapsed_ns:", total_detection_elapsed_ns)
 print("total_detection_count:", total_detection_count)

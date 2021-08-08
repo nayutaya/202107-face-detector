2464c3faafe75bca2114e21a09e20fcfdb1787d6
Yuya Kato <yuyakato@gmail.com>
2021-08-08 22:03:42 +0900

    動画ファイルのハッシュ値を取得する処理を追加

--- video-analyzer/src/main.py
+++ video-analyzer/src/main.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+import hashlib
+import pathlib
 import sys
 
 import cv2
 
-capture = cv2.VideoCapture(sys.argv[1])
+video_path = pathlib.Path(sys.argv[1])
+
+with video_path.open("rb") as file:
+    sha1_hash = hashlib.sha1(file.read()).hexdigest()
+    print(sha1_hash)
+
+capture = cv2.VideoCapture(str(video_path))
 assert capture.isOpened()
 print("CAP_PROP_FRAME_WIDTH:", capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 print("CAP_PROP_FRAME_HEIGHT", capture.get(cv2.CAP_PROP_FRAME_HEIGHT))

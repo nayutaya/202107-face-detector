d129aab0dad23ab70cfc1aea4bd84958bbfcff97
Yuya Kato <yuyakato@gmail.com>
2021-08-08 18:16:32 +0900

    NumPy形式のバイナリデータに対応

--- detector-insightface/src/main.py
+++ detector-insightface/src/main.py
@@ -68,15 +68,19 @@ async def get_root():
 
 @app.post("/detect", response_model=mytypes.DetectResponse)
 async def post_detect(file: fastapi.UploadFile = fastapi.File(...)):
-    # TODO: PNG形式に対応する。
-    # TODO: NumPy形式に対応する。
-    assert file.content_type == "image/jpeg"
-
     image_bin = file.file.read()
     sha1_hash = hashlib.sha1(image_bin).hexdigest()
     file_size = len(image_bin)
-    image_array = np.asarray(bytearray(image_bin), dtype=np.uint8)
-    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+    # TODO: PNG形式に対応する。
+    if file.content_type == "image/jpeg":
+        image_array = np.asarray(bytearray(image_bin), dtype=np.uint8)
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    elif file.content_type == "application/octet-stream":
+        image_io = io.BytesIO(image_bin)
+        image = np.load(image_io)
+    else:
+        assert False  # TODO: ちゃんとエラーを発生させる
 
     start_time_ns = time.perf_counter_ns()
     faces = face_analysis.get(image)

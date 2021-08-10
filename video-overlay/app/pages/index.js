49256308240e9fe684504dcc650fc9fabce4d07e
Yuya Kato <yuyakato@gmail.com>
2021-08-10 23:17:59 +0900

    動画を追加

--- video-overlay/app/pages/index.js
+++ video-overlay/app/pages/index.js
@@ -8,6 +8,11 @@ export default function Page() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>video-overlay</div>
+      <div>
+        <video controls>
+          <source src="/pixabay_76889_960x540.mp4" />
+        </video>
+      </div>
     </div>
   );
 }

ab9037e78b0f6b708b10b2062b69632d0f6d4aa5
Yuya Kato <yuyakato@gmail.com>
2021-08-11 20:25:02 +0900

    タイマ処理を追加

--- video-overlay/app/pages/index.js
+++ video-overlay/app/pages/index.js
@@ -1,11 +1,41 @@
 import Head from "next/head";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Page() {
+  // const [videoState, setVideoState] = useState({paused: true, currentTime: null});
+  const [paused, setPaused] = useState(true);
+  const refTimerId = useRef(null);
+  const videoRef = React.createRef();
+
   const onChanged = (event) => {
     // console.log(e);
     const videoElement = event.target;
-    console.log([videoElement.paused, videoElement.currentTime]);
+    // setVideoState({
+    //   paused: videoElement.paused,
+    //   currentTime: videoElement.currentTime,
+    // })
+    setPaused(videoElement.paused);
+    // console.log("onChanged:", videoRef.current);
   };
+
+  const onTimer = () => {
+    console.log("timer");
+    console.log("onTimer:", videoRef.current);
+  };
+
+  useEffect(() => {
+    // console.log({paused});
+    if ( paused ) {
+      console.log("stop timer");
+      if ( refTimerId.current != null ) {
+        clearInterval(refTimerId.current);
+        refTimerId.current = null;
+      }
+    } else {
+      console.log("start timer");
+      refTimerId.current = setInterval(onTimer, 500);
+    }
+  }, [paused]);
 
   return (
     <div>
@@ -16,6 +46,7 @@ export default function Page() {
       <div>video-overlay</div>
       <div>
         <video
+            ref={videoRef}
             src="/pixabay_76889_960x540.mp4"
             onLoadStart ={(e) => onChanged(e)}
             onCanPlay   ={(e) => onChanged(e)}

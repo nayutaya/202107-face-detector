4a71d20de578d391b8ef95d74120d5d9148c91fc
Yuya Kato <yuyakato@gmail.com>
2021-08-11 21:34:20 +0900

    フレーム番号を状態として保持するように変更

--- video-overlay/app/pages/index.js
+++ video-overlay/app/pages/index.js
@@ -2,41 +2,28 @@ import Head from "next/head";
 import React, { useState, useEffect, useRef, useCallback } from "react";
 
 export default function Page() {
-  console.log("Page");
-  // const refVideo = React.createRef();
   const refVideo = useRef(null);
   const refTimerId = useRef(null);
   const [paused, setPaused] = useState(true);
-  const [currentTime, setCurrentTime] = useState(0.0);
+  const [currentFrameIndex, setCurrentFrameIndex] = useState(0);
+
+  const fps = 29.97;
 
   const onChanged = useCallback((event) => {
     const videoElement = event.target;
     setPaused(videoElement.paused);
-    // setCurrentTime(videoElement.currentTime);
+    setCurrentFrameIndex(Math.floor(videoElement.currentTime * fps));
   }, []);
-
-  /*
-  const onTimer = useCallback(() => {
-    console.log("onTimer:", refVideo.current);
-    if ( refVideo.current != null ) {
-      // setCurrentTime(refVideo.current.currentTime);
-    }
-  }, []);
-  */
 
   const startTimer = useCallback(() => {
-    console.log("start timer");
-    // refTimerId.current = setInterval(onTimer, 500);
     refTimerId.current = setInterval(() => {
-      console.log("onTimer:", refVideo.current);
       if ( refVideo.current != null ) {
-        setCurrentTime(refVideo.current.currentTime);
+        setCurrentFrameIndex(Math.floor(refVideo.current.currentTime * fps));
       }
-    }, 500);
+    }, 33);
   }, [refTimerId, refVideo]);
 
   const stopTimer = useCallback(() => {
-    console.log("stop timer");
     if ( refTimerId.current != null ) {
       clearInterval(refTimerId.current);
       refTimerId.current = null;
@@ -44,7 +31,6 @@ export default function Page() {
   }, [refTimerId]);
 
   useEffect((x) => {
-    // console.log({paused});
     if ( paused ) {
       stopTimer();
     } else {
@@ -59,7 +45,7 @@ export default function Page() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>video-overlay</div>
-      <div>currentTime: {currentTime}</div>
+      <div>currentFrameIndex: {currentFrameIndex}</div>
       <div>
         <video
             ref={refVideo}
